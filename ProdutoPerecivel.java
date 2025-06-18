68c32561d2d0f27e6b1d550685e89ce66609131d
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-06-17 21:38:23 -0300

    Create ProdutoPerecivel.java

---
+++ ProdutoPerecivel.java
@@ -0,0 +1,26 @@
+
+package modelo;
+
+import java.time.LocalDate;
+
+public class ProdutoPerecivel extends Produto 
+{
+    private LocalDate dataDeValidade;
+
+    public ProdutoPerecivel(int id, double precoUnitario, Grandeza grandeza, LocalDate dataDeValidade)
+    {
+        super(id, precoUnitario, grandeza);
+        this.dataDeValidade = dataDeValidade;
+    }
+
+    public LocalDate getDataDeValidade()
+    {
+        return this.dataDeValidade;
+    }
+
+    public int getDiasParaVencimento()
+    {
+        return LocalDate.now().until(this.dataDeValidade).getDays();
+    }
+
+}
