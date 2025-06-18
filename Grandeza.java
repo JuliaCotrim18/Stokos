4a25ba3cc83a51202521cb9cbed1091842686163
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-06-17 21:37:20 -0300

    Create Grandeza.java

---
+++ Grandeza.java
@@ -0,0 +1,11 @@
+package modelo;
+
+// Grandezas em que os produtos podem ser medidos
+// há produtos unitários como lâmpada
+// mas pode haver produtos vendidos por peso ou até em volume 
+public enum Grandeza
+{
+    PESO,
+    UNIDADE,
+    VOLUME;
+}
